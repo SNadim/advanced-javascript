3262c3783c1c47f782411920dc029519acdc5b37
SNadim <93469458+SNadim@users.noreply.github.com>
2021-11-22 16:58:22 +0600

    double triple equal

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,17 +1,17 @@
-// let name;
-// function add(num1, num2) {
-//     console.log(num1 + num2);
-//     return;
-// }
+let name;
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return;
+}
 
-// function add(num1, num2=10) {
-//     console.log(num1, num2);
-// }
+function add(num1, num2=10) {
+    console.log(num1, num2);
+}
 
-// const premik = {name: 'Smart Dude', phone:490290};
-// console.log(premik.gf);
+const premik = {name: 'Smart Dude', phone:490290};
+console.log(premik.gf);
 
-// let fun = undefined;
+let fun = undefined;
 let ages = [2, 5, 9];
 console.log(ages[11]);
 
