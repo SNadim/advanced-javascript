0bab346686243a71aabe42f332e66f37a2c115b6
SNadim <93469458+SNadim@users.noreply.github.com>
2021-11-22 16:46:56 +0600

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+// let name;
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+//     return;
+// }
+
+// function add(num1, num2=10) {
+//     console.log(num1, num2);
+// }
+
+// const premik = {name: 'Smart Dude', phone:490290};
+// console.log(premik.gf);
+
+// let fun = undefined;
+let ages = [2, 5, 9];
+console.log(ages[11]);
+
+
