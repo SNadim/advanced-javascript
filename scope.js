6b3dffffbc92373553617785ac8df899f831e4c6
SNadim <93469458+SNadim@users.noreply.github.com>
2021-11-23 09:32:29 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,21 @@
+let bonus = 20;
+
+function sum(first, second) {
+    var result = first + second + bonus;
+    //console.log(bonus);
+    if(result > 9) {
+        var mood = "happy";
+        mood = "fishy";
+        mood = "funky";
+        mood = "cranky";
+        console.log(mood);
+    }
+    console.log(mood);
+    return result;
+}
+
+console.log(day);
+var day = "friday";
+const output = sum(3, 7);
+// console.log(bonus);
+// console.log(output);
